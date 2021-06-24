cae08a04cfae86f6f6a47f0eaee71d2bbeea3c47
Projetphp <alexandre.capaldi@hotmail.fr>
2021-06-24 21:14:28 +0200

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
 class App extends React.Component {
   render() {
+    console.log("json=", contacts)
     return (
       <div className="App" >
+
+        {contacts.map((el) => (<table>
+          <p>{el.name}</p>
+          <img src={el.pictureUrl}></img>
+          <p>{el.popularity}</p>
+        </table>)).splice(0, 5)}
       </div>
     );
   }
