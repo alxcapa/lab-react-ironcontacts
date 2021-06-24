1f0f5653020cbbd7b0d48fbeab10b204fb153379
potauro <karinagonzalezsanchez@gmail.com>
2021-06-24 21:32:21 +0200

    iteration 2 (not all of it)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './App.css';
 import contacts from './contacts.json';
 
+
 class App extends React.Component {
+
+ addContact = () => {
+   let random = Math.floor(Math.random() * contacts.length)
+   let randomresult = contacts[random]
+   console.log(randomresult)
+ }
+
+
   render() {
     console.log("json=", contacts)
     return (
+      
       <div className="App" >
-
+      <button onClick={this.addContact}>Add Random Contact</button>
         {contacts.map((el) => (<table>
           <p>{el.name}</p>
           <img src={el.pictureUrl}></img>
