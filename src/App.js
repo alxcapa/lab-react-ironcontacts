580e43e149fc888668ada6a3589e12960ca106d2
potauro <karinagonzalezsanchez@gmail.com>
2021-06-26 11:45:21 +0200

    iterations 2 & 3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,66 @@ import contacts from './contacts.json';
 
 class App extends React.Component {
 
+  state ={
+    contacts: contacts,
+    first5: [...contacts].splice(0, 5)
+  }
+
+
+
  addContact = () => {
-   let random = Math.floor(Math.random() * contacts.length)
-   let randomresult = contacts[random]
-   console.log(randomresult)
+   let difference = this.state.contacts.filter(x => !this.state.first5.includes(x));
+ console.log('difference =',difference)
+   let random = Math.floor(Math.random() * difference.length)
+   let randomResult = difference[random]
+
+   console.log(randomResult)
+   this.setState ({
+    first5: [...this.state.first5, randomResult]
+   })
+  
  }
+
+ sortByName = () => {
+  let sortedNames = this.state.first5.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;
+})
+console.log ('sorted names =',sortedNames);
+
+this.setState ({
+  first5: sortedNames
+})
+
+ }
+ 
+
+
+sortByPop = () => {
+  let sortedPop = this.state.first5.sort(function(a, b){
+    if(a.popularity > b.popularity) { return -1; }
+    if(a.popularity < b.popularity) { return 1; }
+    return 0;
+})
+console.log ('sorted pop =',sortedPop);
+
+this.setState ({
+  first5: sortedPop
+})
+   
+ }
+ 
+ deleteContact = () => {
+//   //let difference = this.state.contacts.filter(x => !this.state.first5.includes(x));
+// console.log('difference =',difference)
+
+
+//   this.setState ({
+//    first5: [...this.state.first5, randomResult]
+//   })
+ 
+}
 
 
   render() {
@@ -18,11 +73,13 @@ class App extends React.Component {
       
       <div className="App" >
       <button onClick={this.addContact}>Add Random Contact</button>
-        {contacts.map((el) => (<table>
+      <button onClick={this.sortByName}>Sort by Name</button>
+      <button onClick={this.sortByPop}>Sort by Popularity</button>
+        {this.state.first5.map((el) => (<table>
           <p>{el.name}</p>
           <img src={el.pictureUrl}></img>
           <p>{el.popularity}</p>
-        </table>)).splice(0, 5)}
+        </table>))}
       </div>
     );
   }
